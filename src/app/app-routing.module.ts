a02b3d2cceb670aee0d1ceb9dcd4413c8c4bef35
wilham.sousa <wilham.sousa@rasini.com.br>
2017-09-29 19:29:59 -0300

    inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './shared';
+import { ClienteConveniadoComponent } from 'app/cliente-conveniado/cliente-conveniado.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        loadChildren: './layout/layout.module#LayoutModule',
+        canActivate: [AuthGuard]
+    },
+    { path: 'cliente-conveniado', component: ClienteConveniadoComponent },
+    { path: 'login', loadChildren: './login/login.module#LoginModule' },
+    { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
+    { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
+    { path: '**', redirectTo: 'not-found' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ src/app/cliente-conveniado/cliente-conveniado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-conveniado',
+  templateUrl: './cliente-conveniado.component.html',
+  styleUrls: ['./cliente-conveniado.component.scss']
+})
+export class ClienteConveniadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
